d95f74ea35f5b7e2f9cfe34f20fd579496e9828a
unknown <jake.park2398@gmail.com>
2018-10-15 23:21:24 -0700

    Mostly done, need to clean up some bugs

--- exp_eval.py
+++ exp_eval.py
@@ -4,6 +4,8 @@ from stack_array import Stack
 class PostfixFormatException(Exception):
     pass
 
+
+
 def postfix_eval(input_str):
     """Evaluates a postfix expression"""
 
@@ -11,7 +13,137 @@ def postfix_eval(input_str):
     are space separated.  Tokens are either operators + - * / ^ or numbers
     Returns the result of the expression evaluation. 
     Raises an PostfixFormatException if the input is not well-formed"""
-    pass
+    s = Stack(30)
+    token = ''
+    i = 0
+    ib = 0
+    num_op = 0
+    num_num = 0
+    while ib < len(input_str):
+        char = input_str[ib]
+        if char == ' ':
+            ib += 1
+        elif char == '.':
+            ib += 1
+        elif char == '+' or char == '-' or char == '*' or char == '/' or char == '<' or char == '>':
+            if char == '*':
+                if ib < len(input_str) - 1:
+                    if input_str[ib + 1] == '*':
+                        num_op += 1
+                        ib += 2
+                else:
+                    num_op += 1
+                    ib += 1
+            elif char == '<' and ib < len(input_str) - 1:
+                if input_str[ib + 1] == '<':
+                    num_op += 1
+                    ib += 2
+            elif char == '>' and ib < len(input_str) - 1:
+                if input_str[ib + 1] == '>':
+                    num_op += 1
+                    ib += 2
+            else:
+                num_op += 1
+                ib += 1
+        else:
+            try:
+                int(char)
+                if ib == len(input_str) - 1:
+                    num_num += 1
+                    ib += 1
+                elif input_str[ib + 1] == ' ':
+                    num_num += 1
+                    ib += 1
+                elif input_str[ib + 1] == '.':
+                    ib += 1
+            except ValueError as e:
+                raise PostfixFormatException("Invalid token")
+    if num_op + 1 < num_num:
+        raise PostfixFormatException("Too many operands")
+    if num_op + 1 > num_num:
+        raise PostfixFormatException("Insufficient operands")
+    while i < len(input_str):
+        char = input_str[i]
+        if char == ' ':
+            if token is not '':
+                s.push(token)
+                token = ''
+            i += 1
+        elif char == '+':
+                i2 = int(s.pop())
+                i1 = int(s.pop())
+                res = str(i1 + i2)
+                s.push(res)
+                token = ''
+                i += 1
+        elif char == '-':
+            i2 = int(s.pop())
+            i1 = int(s.pop())
+            res = str(i1 - i2)
+            s.push(res)
+            token = ''
+            i += 1
+        elif char == '*':
+            if i < len(input_str) - 1:
+                if input_str[i + 1] == '*':
+                    i2 = int(s.pop())
+                    i1 = int(s.pop())
+                    res = str(i1 ** i2)
+                    s.push(res)
+                    token = ''
+                    i += 2
+            else:
+                i2 = int(s.pop())
+                i1 = int(s.pop())
+                res = str(i1 * i2)
+                s.push(res)
+                token = ''
+                i += 2
+        elif char == '/':
+            i2 = int(s.pop())
+            if i2 == 0:
+                raise ValueError('Cannot divide by 0!')
+            i1 = int(s.pop())
+            res = str(i1 / i2)
+            s.push(res)
+            token = ''
+            i += 1
+        elif char == '<':
+            if input_str[i + 1] == '<':
+                try:
+                    i2 = int(s.pop())
+                    i1 = int(s.pop())
+                    res = str(i1 << i2)
+                    s.push(res)
+                    token = ''
+                    i += 2
+                except ValueError:
+                    raise PostfixFormatException('Invalid bit shift operand')
+        elif char == '>':
+            if input_str[i + 1] == '>':
+                try:
+                    i2 = int(s.pop())
+                    i1 = int(s.pop())
+                    res = str(i1 >> i2)
+                    s.push(res)
+                    token = ''
+                    i += 2
+                except ValueError:
+                    raise PostfixFormatException('Invalid bit shift operand')
+            else:
+                raise PostfixFormatException('Invalid token')
+        elif char == '.':
+            token += char
+            i += 1
+        elif int(char) is not ValueError:
+            token += char
+            i += 1
+    try:
+        pop = s.pop()
+        result = int(pop)
+    except ValueError:
+        result = float(pop())
+    return result
 
 def infix_to_postfix(input_str):
     """Converts an infix expression to an equivalent postfix expression"""
@@ -19,7 +151,80 @@ def infix_to_postfix(input_str):
     """Input argument:  a string containing an infix expression where tokens are 
     space separated.  Tokens are either operators + - * / ^ parentheses ( ) or numbers
     Returns a String containing a postfix expression """
-    pass
+    s = Stack(30)
+    i = 0
+    post = ''
+    operators = ['+','-','*','/','^','<<','>>','**','**']
+    highest = ['<<','>>']
+    high = ['**']
+    medium = ['*','/']
+    low = ['+','-']
+    nums = ['1','2','3','4','5','6','7','8','9','0']
+    while i < len(input_str):
+        char = input_str[i]
+        if char == ' ':
+            i += 1
+        elif post == '' and char in nums:
+            post += char
+            i += 1
+        elif char in nums:
+            post += ' ' + char
+            i += 1
+        elif char == '(':
+            s.push(char)
+            i += 1
+        elif s.is_empty():
+            s.push(char)
+            i += 1
+        elif char == ')':
+            if s.peek() != '(':
+                while s.peek() != '(':
+                    post += " " + s.pop()
+            s.pop()
+            i += 1
+        elif char == '<':
+            if input_str[i + 1] == '<':
+                o2 = s.peek()
+                if o2 in highest:
+                    post += " " + s.pop()
+                s.push(char + input_str[i+1])
+                i += 2
+        elif char == '>':
+            if input_str[i + 1] == '>':
+                o2 = s.peek()
+                if o2 in highest:
+                    post += " " + s.pop()
+                s.push(char + input_str[i+1])
+                i += 2
+        elif char == '*':
+            if input_str[i + 1] == '*':
+                o2 = s.peek()
+                if o2 in highest:
+                    post = " " + s.pop()
+                s.push('**')
+                i += 2
+            else:
+                o2 = s.peek()
+                if o2 in highest or o2 in high or o2 in medium:
+                    post += " " + s.pop()
+                s.push(char)
+                i += 1
+        elif char == '/':
+            o2 = s.peek()
+            if o2 in highest or o2 in high or o2 in medium:
+                post += " " + s.pop()
+            s.push(char)
+            i += 1
+        elif char in low:
+            o2 = s.peek()
+            if o2 in highest or o2 in high or o2 in medium or o2 in low:
+                post += " " + s.pop()
+            s.push(char)
+            i += 1
+    while not s.is_empty():
+        post += " " + s.pop()
+    return post
+
 
 
 def prefix_to_postfix(input_str):
@@ -27,6 +232,49 @@ def prefix_to_postfix(input_str):
     """Input argument: a string containing a prefix expression where tokens are 
     space separated.  Tokens are either operators + - * / ^ parentheses ( ) or numbers
     Returns a String containing a postfix expression(tokens are space separated)"""
-    pass
+    s = Stack(30)
+    i = len(input_str) - 1
+    inf = ''
+    operators = ['+','-','*','/','<','>','**','**']
+    highest = ['<','>']
+    high = ['**']
+    medium = ['*','/']
+    low = ['+','-']
+    nums = ['1','2','3','4','5','6','7','8','9','0']
+    while i >= 0:
+        char = input_str[i]
+        if char == ' ':
+            i -= 1
+        elif char in nums:
+            s.push(char)
+            i -= 1
+        elif char in operators:
+            if char == '*':
+                if i > 1:
+                    if input_str[i - 1] == '*':
+                        op1 = s.pop()
+                        op2 = s.pop()
+                        inf = op1 + ' ' + op2 + ' ' + '**'
+                        s.push(inf)
+                        i -= 2
+                else:
+                    op1 = s.pop()
+                    op2 = s.pop()
+                    inf = op1 + ' ' + op2 + ' ' + char
+                    s.push(inf)
+                    i -= 1
+            elif char == '<' or char == '>':
+                op1 = s.pop()
+                op2 = s.pop()
+                inf = op1 + ' ' + op2 + ' ' + input_str[i-1] + char
+                s.push(inf)
+                i -= 2
+            else:
+                op1 = s.pop()
+                op2 = s.pop()
+                inf = op1 + ' ' + op2 + ' ' + char
+                s.push(inf)
+                i -= 1
+    return s.pop()
 
-
+print(postfix_eval('3 1.0 >>'))
--- exp_eval_tests.py
+++ exp_eval_tests.py
@@ -4,36 +4,63 @@ import unittest
 from exp_eval import *
 
 class test_expressions(unittest.TestCase):
-    def test_postfix_eval_01(self):
+    def test_postfix_eval_simple(self):
         self.assertAlmostEqual(postfix_eval("3 5 +"), 8)
+        self.assertAlmostEqual(postfix_eval("3 2 -"), 1)
+        self.assertAlmostEqual(postfix_eval("3 5 *"), 15)
+        self.assertAlmostEqual(postfix_eval("3 1 /"), 3.0)
+        self.assertAlmostEqual(postfix_eval("3 2 **"), 9)
+        self.assertAlmostEqual(postfix_eval('2 1 <<'), 4)
+        self.assertAlmostEqual(postfix_eval('2 1 >>'), 1)
 
-    def test_postfix_eval_02(self):
+
+    def test_postfix_eval_invalid_token(self):
         try:
             postfix_eval("blah")
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Invalid token")
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 4 e')
 
-    def test_postfix_eval_03(self):
+    def test_postfix_eval_insufficient_operands(self):
         try:
             postfix_eval("4 +")
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Insufficient operands")
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 4 + 2 / +')
 
-    def test_postfix_eval_04(self):
+    def test_postfix_eval_too_many_operands(self):
         try:
             postfix_eval("1 2 3 +")
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Too many operands")
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 4 + 2')
+
+    def test_postfix_eval_invalid_bitshift(self):
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 1.0 <<')
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 1.0 >>')
+
+    def test_postfix_eval_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            postfix_eval('3 0 /')
+        with self.assertRaises(ValueError):
+            postfix_eval('3 3 - 0 /')
 
     def test_infix_to_postfix_01(self):
         self.assertEqual(infix_to_postfix("6 - 3"), "6 3 -")
         self.assertEqual(infix_to_postfix("6"), "6")
-        
+        self.assertEqual(infix_to_postfix('3 + 4 * 2 / ( 1 - 5 ) ** 2 ** 3'), '3 4 2 * 1 5 - 2 3 ** ** / +')
+
     def test_prefix_to_postfix(self):
         self.assertEqual(prefix_to_postfix("* - 3 / 2 1 - / 4 5 6"), "3 2 1 / - 4 5 / 6 - *")
+        self.assertEqual(prefix_to_postfix('+ << 3 4 >> 5 6'), '3 4 << 5 6 >> +')
 
 if __name__ == "__main__":
     unittest.main()
